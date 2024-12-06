f5bc8ce24b7271882cdc13c8decb3ecf934af6f3
Christian Obora <124216068+christianobora@users.noreply.github.com>
2024-12-05 21:16:40 -0600

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mailstream",
-    version="0.1.0",
+    version="0.1.1",
     author="Christian Obora",
     author_email="christianobora@uchicago.edu",
     description="Async IMAP client for streaming email messages",
